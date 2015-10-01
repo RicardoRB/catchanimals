2f684e9962bb4d01c761ada2e4c879afc485f1dc
RicardoRB <ricardoromebeni@gmail.com>
2015-10-01 20:30:49 +0200

    Added constants class. Forgotten

---
+++ core/src/com/ricardorb/catchanimals/Constants.java
@@ -0,0 +1,38 @@
+package com.ricardorb.catchanimals;
+
+public class Constants {
+	
+	//General
+	public static final int WINDOWY = 480;
+	public static final int WINDOWX = 800;
+	
+	//GameScreen
+	public static final String BTNEXITGAME = "X";
+	public static final String BTNOPTIONS = "O";
+	public static final String TXTSCORE = "Score: ";
+	public static final String TXTGAMEOVER = "Game Over";
+	public static final String TXTYOURSCORE = "Your score";
+	public static final String TXTTIME = "Time: ";
+	public static final String TXTPAUSE = "Pause";
+		//Dialogs
+		public static final String TXTEXITASK = "Are you sure you want to quit?";
+		public static final String TXTYES = "Yes";
+		public static final String TXTNO = "No";
+		public static final String TXTBACKTOMENU = "Back to menu";
+		public static final String TXTRETRY = "Retry?";
+		public static final String TXTRESUMEASK = "Resume game?";
+		public static final String TXTRESUME = "Resume";
+	
+	//MainMenuScreen
+	public static final String TXTSTARTGAME = "Start Game";
+	public static final String TXTEXIT = "Exit";
+	public static final String TXTOPTIONS = "Options";
+	public static final String TXTMUSIC = "Music";
+	public static final String TXTEFFECTS = "Effects";
+	
+	//OptionScreen
+	public static final String TXTBOTTOMBAR = "Bottom bar";
+	public static final String TXTMUSICONOFF = "Turn music On/Off";
+	public static final String TXTEFFECTSONOFF = "Turn effects On/Off";
+	public static final String TXTBACK = "Back";
+}
